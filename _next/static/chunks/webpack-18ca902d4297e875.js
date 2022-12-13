dcac44e0cfa16b928aef42a7be2217506c10addc
bramz1212 <merahhh951@gmail.com>
2022-12-13 20:11:14 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-18ca902d4297e875.js
+++ _next/static/chunks/webpack-18ca902d4297e875.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "872e680f82c29d69" + ".css";
+/******/ 			return "static/css/" + "ce725e14fca9d94f" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
